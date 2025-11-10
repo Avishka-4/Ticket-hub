6fcf7041b43b67bf3afdd2e8b538f09b836b2993
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2025-11-10 23:16:18 +0530

    edited payment page

--- pages/order.php
+++ pages/order.php
@@ -37,17 +37,13 @@
     <label for="inputPassword4" class="form-label">Email Address</label>
     <input type="email" class="form-control" id="email" placeholder="Enater Your Email Address">
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
     <label for="inputPassword4" class="form-label">Phone Number</label>
     <input type="number" class="form-control" id="phone" placeholder="Enater Your Phone Number">
   </div>
-  <div class="col-md-4">
+  <div class="col-md-3">
     <label for="inputPassword4" class="form-label">Number of Members</label>
     <input type="number" min="1" max="20" class="form-control" id="members">
-  </div>
-  <div class="col-md-4">
-    <label for="inputPassword4" class="form-label">Email Address</label>
-    <input type="email" class="form-control" id="email" placeholder="Enater Your Email Address">
   </div>
   <div class="col-md-6">
     <label for="inputEmail4" class="form-label">Departure Date</label>
@@ -57,28 +53,25 @@
     <label for="inputPassword4" class="form-label">Return Date</label>
     <input type="date" class="form-control" id="date">
   </div>
+   <div class="col-md-6">
+    <label for="inputState" class="form-label">Payment Mothod</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose Your Payment Mothod</option>
+      <option>Debit Card</option>
+      <option>Credi Card</option>
+    </select>
+  </div>
   <div class="col-12">
     <label for="inputAddress" class="form-label">Account Number</label>
     <input type="" class="form-control" id="inputAddress" placeholder="Please Enter Your Account Number">
   </div>
-  <div class="col-12">
+  <div class="col-9">
     <label for="inputAddress2" class="form-label">Account Holder Name</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Please Enter Account Holder Name">
   </div>
-  <div class="col-md-6">
+  <div class="col-md-3">
     <label for="inputCity" class="form-label">CVV</label>
     <input type="number" class="form-control" id="inputCity" placeholder="CVV">
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Number Of Members</label>
-    <input type="number" min="1" max="20" class="form-control" id="inputZip">
   </div>
   <div class="col-12">
     <div class="form-check">
